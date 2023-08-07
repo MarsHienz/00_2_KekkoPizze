5a9bf616276959ee6f5d5bae20a8e11016e79be9
Mars <141424089+MarsHienz@users.noreply.github.com>
2023-08-07 15:25:06 +0200

    Add files via upload

---
+++ data/pizzaData.js
@@ -0,0 +1,46 @@
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino Piccante",
+    ingredients: "Tomato, mozarella, salamino, diocane",
+    price: 98.3,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, dio, cane",
+    price: 1200,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+];
+
+export default pizzaData;
