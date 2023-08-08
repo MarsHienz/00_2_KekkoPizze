570731ace0517404034d5a3058a41141faf4866b
Mars <141424089+MarsHienz@users.noreply.github.com>
2023-08-08 18:20:26 +0200

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function Footer() {
+        return (
+            <div class="bg-[#edc84b] h-14 flex justify-center items-center">
+                <h3>© 2023 Scott Chen. All rights reserved</h3>
+
+            </div>
+        )
+    }
+    export default Footer;
